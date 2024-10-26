fbd676b56fde4d5700b8198ec910e72f184058db
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-10-25 21:24:42 -0300

    Implementacion del servicio user

---
+++ src/main/java/com/nahudev/electronic_shop/service/user/IUserService.java
@@ -0,0 +1,17 @@
+package com.nahudev.electronic_shop.service.user;
+
+import com.nahudev.electronic_shop.model.User;
+import com.nahudev.electronic_shop.request.CreateUserRequest;
+import com.nahudev.electronic_shop.request.UpdateUserRequest;
+
+public interface IUserService {
+
+    public User getUserById(Long userId);
+
+    public User createUser(CreateUserRequest request);
+
+    public User updateUser(UpdateUserRequest request, Long userId);
+
+    public void deleteUser(Long userId);
+
+}
